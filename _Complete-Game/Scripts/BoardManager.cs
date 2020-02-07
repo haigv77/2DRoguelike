c8b7daaab7abbb63f93e1533b9cc1889fa194330
haigv77 <haigv77@gmail.com>
2020-02-07 17:09:20 +0700

    Adding UI & Level Transitions

--- _Complete-Game/Scripts/BoardManager.cs
+++ _Complete-Game/Scripts/BoardManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic; 		//Allows us to use Lists.
 using Random = UnityEngine.Random; 		//Tells Random to use the Unity Engine random number generator.
 
 namespace Completed
-	
 {
 	
 	public class BoardManager : MonoBehaviour
